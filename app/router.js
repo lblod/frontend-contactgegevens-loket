84215e612df87e1c31d319186fe90ccfc9b936b8
Oscar <lagartoverde97@gmail.com>
2023-09-15 12:34:22 +0200

    finished mock-login

--- app/router.js
+++ app/router.js
@@ -8,4 +8,8 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('mock-login');
+  this.route('auth', { path: '/authorization' }, function () {
+    this.route('logout');
+  });
+  this.route('test');
 });
