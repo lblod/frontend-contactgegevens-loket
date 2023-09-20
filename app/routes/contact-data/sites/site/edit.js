739d358845c5c6e46c35bca55239befc39a31d03
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-09-20 12:23:48 +0200

    WIP : Site validation

--- app/routes/contact-data/sites/site/edit.js
+++ app/routes/contact-data/sites/site/edit.js
@@ -1,7 +1,16 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { getSiteValidations } from 'frontend-contactgegevens-loket/validations/sites';
+import { getAddressValidations } from 'frontend-contactgegevens-loket/validations/address';
+import contactValidations from 'frontend-contactgegevens-loket/validations/contact-point';
+import secondaryContactValidations from 'frontend-contactgegevens-loket/validations/secondary-contact-point';
 import { createValidatedChangeset } from 'frontend-contactgegevens-loket/utils/changeset';
+// import {
+//   createPrimaryContact,
+//   createSecondaryContact,
+//   findPrimaryContact,
+//   findSecondaryContact,
+// } from 'frontend-contactgegevens-loket/models/contact-point';
 
 export default class ContactDataSitesSiteEditRoute extends Route {
   @service currentSession;
@@ -17,20 +26,39 @@ export default class ContactDataSitesSiteEditRoute extends Route {
   }
 
   async model() {
-    const sites = this.modelFor('contact-data.sites');
+    // the following code is a workaround for the mock data
+    let sites = this.modelFor('contact-data.sites');
     let { id: siteId } = this.paramsFor('contact-data.sites.site');
-    const site = sites['sites'].find((site) => site.id === siteId);
-    const primaryContact = site['contacts'].find(
+    let site = sites['sites'].find((site) => site.id === siteId);
+    let address = site.address;
+    let contact = site['contacts'].find(
       (contact) => contact.type === 'Primary',
     );
-    const secondaryContact = site['contacts'].find(
+    let secondaryContact = site['contacts'].find(
       (contact) => contact.type === 'Secondary',
     );
+
+    // let contact = findPrimaryContact(contacts); // uncomment when backend works
+
+    if (!contact) {
+      // contact = createPrimaryContact(this.store); // uncomment when backend works
+      contact = true; // delete this when backend works
+    }
+
+    if (!secondaryContact) {
+      // secondaryContact = createSecondaryContact(this.store); // uncomment when backend works
+      secondaryContact = true; // change this when backend works
+    }
+
     return {
       site: createValidatedChangeset(site, getSiteValidations()),
       sites,
-      primaryContact,
-      secondaryContact,
+      contact: createValidatedChangeset(contact, contactValidations),
+      secondaryContact: createValidatedChangeset(
+        secondaryContact,
+        secondaryContactValidations,
+      ),
+      address: createValidatedChangeset(address, getAddressValidations()),
     };
   }
 }
