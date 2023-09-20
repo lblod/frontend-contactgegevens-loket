dd2828c41c679c8b978f50a93fee16b8e98b37fc
Aad Versteden <madnificent@gmail.com>
2023-09-20 13:42:35 +0200

    Hacking in dynamic data
    Yay for data coming from the backend

--- app/routes/contact-data/sites/site.js
+++ app/routes/contact-data/sites/site.js
@@ -4,22 +4,15 @@ import { inject as service } from '@ember/service';
 export default class ContactDataSitesSiteIndexRoute extends Route {
   @service store;
 
-  async model() {
-    const sites = this.modelFor('contact-data.sites');
-    let { id: siteId } = this.paramsFor('contact-data.sites.site');
-    const site = sites['sites'].find((site) => site.id === siteId);
-    const primaryContact = site['contacts'].find(
-      (contact) => contact.type === 'Primary',
+  async model({id}) {
+    const site = await this.store.findRecord('site', id);
+    const sites = [];
+
+    const primaryContact = (await site.contacts).find(
+      (contact) => contact.type === 'Primary'
     );
-    const secondaryContact = site['contacts'].find(
-      (contact) => contact.type === 'Secondary',
-    );
-    console.log(
-      this.store
-        .findRecord('site', '981a82bb-f144-402c-8445-474f62b67a24')
-        .then((site) => {
-          console.log(site.get('address'));
-        }),
+    const secondaryContact = (await site.contacts).find(
+      (contact) => contact.type === 'Secondary'
     );
     return { site, sites, primaryContact, secondaryContact };
   }
