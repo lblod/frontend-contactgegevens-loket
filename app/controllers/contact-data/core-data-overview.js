604481865275d839364063db446375f3b4e922bf
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2023-09-28 12:00:11 +0200

    Core data page now working nicely. Finally back end issues are solved. Also made the core data display a little more resilient when the data is not present in the database.

--- app/controllers/contact-data/core-data-overview.js
+++ app/controllers/contact-data/core-data-overview.js
@@ -3,16 +3,4 @@ import { inject as service } from '@ember/service';
 
 export default class ContactDataCoreDataOverviewController extends Controller {
   @service currentSession;
-
-  queryParams = ['fake'];
-
-  fake = false;
-
-  get hasContactData() {
-    return (
-      this.model.address &&
-      this.model.primaryContact &&
-      this.model.secondaryContact
-    );
-  }
 }
