5aeb15d163205febb9aebf548c5fa5c733ed421f
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-10-09 17:06:47 +0200

    WIP: Save edit form. Site type and primary site can't be saved yet

--- app/controllers/sites/site/edit.js
+++ app/controllers/sites/site/edit.js
@@ -1,22 +1,46 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { dropTask } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
+import { action } from '@ember/object';
+import { combineFullAddress } from 'frontend-contactgegevens-loket/models/address';
 
 export default class ContactDataEditSiteController extends Controller {
   @service router;
 
-  @dropTask
-  *save(event) {
+  save = task(async (event) => {
     event.preventDefault();
-
     const { site, address, primaryContact, secondaryContact } = this.model;
+    address.fullAddress = combineFullAddress(address);
 
-    const functionCalls = [
-      yield site.validate(),
-      yield address.validate(),
-      yield primaryContact.validate(),
-      yield secondaryContact ? secondaryContact.validate() : null,
+    const validateCalls = [
+      site.validate(),
+      address.validate(),
+      primaryContact.validate(),
+      secondaryContact ? secondaryContact.validate() : null,
     ];
-    yield Promise.all(functionCalls);
+
+    const saveCalls = [
+      site.save(),
+      address.save(),
+      primaryContact.save(),
+      secondaryContact ? secondaryContact.save() : null,
+    ];
+    await Promise.all(validateCalls);
+
+    if (
+      address.isValid &&
+      primaryContact.isValid &&
+      site.isValid &&
+      secondaryContact.isValid
+    ) {
+      await Promise.allSettled(saveCalls);
+      this.router.transitionTo('sites.site.index');
+    }
+  });
+
+  @action
+  cancel(event) {
+    event.preventDefault();
+    this.router.transitionTo('sites.site.index');
   }
 }
