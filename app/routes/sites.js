bb8db1d2749ab4692b0911acbaaea93c088158bd
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-10-16 16:01:15 +0200

    fix: bug where primary site gets duplicated

--- app/routes/sites.js
+++ app/routes/sites.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { sort } from '@ember/object/computed';
 
 export default class ContactDataSitesOverviewRoute extends Route {
   @service store;
@@ -7,11 +8,11 @@ export default class ContactDataSitesOverviewRoute extends Route {
 
   async model() {
     // TODO: Not every site seems to get an address? Why?
-    const sites = await this.currentSession.group.get('sites');
+    let sites = await this.currentSession.group.get('sites');
     const primarySite = await this.currentSession.group.get('primarySite');
 
     return {
-      sites: [primarySite, ...sites],
+      sites,
       primarySiteId: primarySite.id,
     };
   }
