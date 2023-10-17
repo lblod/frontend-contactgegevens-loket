291ddc77871503e315c03abe6cdd26d6dfa9571a
youssef <youssef.jouhar1998@hotmail.com>
2023-10-17 10:54:16 +0200

    bug fix for primary site fixed

--- app/routes/sites.js
+++ app/routes/sites.js
@@ -7,10 +7,12 @@ export default class ContactDataSitesOverviewRoute extends Route {
 
   async model() {
     // TODO: Not every site seems to get an address? Why?
-    const sites = await this.currentSession.group.get('sites', {
-      include: ['address', 'siteType', 'primarySite'],
-    });
+    let sites = await this.currentSession.group.get('sites');
+    const primarySite = await this.currentSession.group.get('primarySite');
 
-    return { sites };
+    return {
+      sites: [...sites, primarySite],
+      primarySiteId: primarySite.id,
+    };
   }
 }
