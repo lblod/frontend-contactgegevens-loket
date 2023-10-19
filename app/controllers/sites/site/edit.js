11663534427af744e1680fab5284954bf9242874
Andreo Van Onckelen <andreo141mlg@gmail.com>
2023-10-19 14:09:05 +0200

    resolve merge conflict

--- app/controllers/sites/site/edit.js
+++ app/controllers/sites/site/edit.js
@@ -45,8 +45,36 @@ export default class ContactDataEditSiteController extends Controller {
       nonPrimarySites.removeObject(site);
     }
 
-    // Now everything is ok we can save away
-    address.fullAddress = combineFullAddress(address);
+    const nonPrimarySites = await adminUnit.sites;
+    const previousPrimarySite = await adminUnit.primarySite;
+    console.log('nonPrimarySites.length before saving', nonPrimarySites.length);
+    console.log('this.isPrimarySite', this.isPrimarySite);
+
+    // we select yes for the primary site
+    if (this.isPrimarySite) {
+      // if there is a previous primary site
+      if (previousPrimarySite) {
+        console.log(
+          'previousPrimarySite.address',
+          previousPrimarySite.address.get('fullAddress'),
+        );
+        console.log(
+          'new primary site address:',
+          site.address.get('fullAddress'),
+        );
+        // check if the previous primary site is in the nonPrimarySites array, if not , add it
+        if (!nonPrimarySites.includes(previousPrimarySite)) {
+          nonPrimarySites.push(previousPrimarySite);
+        }
+        console.log('nonPrimarySites.length', nonPrimarySites.length);
+
+        adminUnit.primarySite = site; // set the new primary site
+      }
+    } else {
+      if (!nonPrimarySites.includes(site)) {
+        nonPrimarySites.push(site);
+      }
+    }
     await site.save();
     await address.save();
     await primaryContact.save();
