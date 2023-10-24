fd401644acd12fee5a91d06ca7c16f01188c7ed4
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2023-10-24 15:22:27 +0200

    Login bug fix: Refactoring of mock login controller to new async task . Temporary bypass of login function offered by the mock-login plugin (from the component). A newer version of mock-login is needed.

--- app/controllers/mock-login.js
+++ app/controllers/mock-login.js
@@ -1,48 +1,57 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { restartableTask, task, timeout } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
+import { action } from '@ember/object';
 
 export default class MockLoginController extends Controller {
   @service store;
+  @service currentSession;
+  @service session;
 
   queryParams = ['gemeente', 'page'];
+  size = 10;
+
   @tracked model;
   @tracked gemeente = '';
   @tracked page = 0;
-  size = 10;
 
-  @task
-  *queryStore() {
+  queryStore = task(async () => {
     const filter = { provider: 'https://github.com/lblod/mock-login-service' };
     if (this.gemeente) {
       filter.user = {
         'family-name': this.gemeente,
       };
     }
-    const accounts = yield this.store.query('account', {
+    const accounts = await this.store.query('account', {
       include: 'user,user.groups',
       filter: filter,
       page: { size: this.size, number: this.page },
       sort: 'user.first-name',
     });
     return accounts;
+  });
+
+  @action
+  async login(account) {
+    // TODO: Error handling
+    const user = await account.user;
+    const group = (await user.groups)[0];
+    const groupId = (await group).id;
+    // TODO: Make new version of mock-login
+    // await loginFunction(account.id, groupId); // Old code using old async tasks.
+    await this.session.authenticate(
+      'authenticator:mock-login',
+      account.id,
+      groupId,
+    );
   }
 
-  @task
-  *callLogin(loginFunction, account) {
-    const user = yield account.user;
-    const group = (yield user.groups)[0];
-    const groupId = (yield group).id;
-    loginFunction(account.id, groupId);
-  }
-
-  @restartableTask
-  *updateSearch(value) {
-    yield timeout(500);
+  updateSearch = task({ restartable: true }, async (value) => {
+    await timeout(500); // Debounce
     this.page = 0;
     this.gemeente = value;
 
-    this.model = yield this.queryStore.perform();
-  }
+    this.model = await this.queryStore.perform();
+  });
 }
