2c1820f63c6ecabe4e200d22add0fdaf049f6fa3
youssef <youssef.jouhar1998@hotmail.com>
2023-10-26 14:59:25 +0200

    Fixed the bug of IGS

--- app/controllers/core-data/admin-unit/index.js
+++ app/controllers/core-data/admin-unit/index.js
@@ -1,31 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { CLASSIFICATION_CODE } from 'frontend-contactgegevens-loket/models/administrative-unit-classification-code';
-
 export default class CoreDataAdminUnitIndexController extends Controller {
   @service currentSession;
-
-  get isMunicipality() {
-    return (
-      this.model.administrativeUnit.classification?.get('id') ===
-      CLASSIFICATION_CODE.MUNICIPALITY
-    );
-  }
-
-  get isIGS() {
-    const typesThatAreIGS = [
-      CLASSIFICATION_CODE.PROJECTVERENIGING,
-      CLASSIFICATION_CODE.DIENSTVERLENENDE_VERENIGING,
-      CLASSIFICATION_CODE.OPDRACHTHOUDENDE_VERENIGING,
-      CLASSIFICATION_CODE.OPDRACHTHOUDENDE_VERENIGING_MET_PRIVATE_DEELNAME,
-    ];
-    return typesThatAreIGS.includes(
-      this.model.administrativeUnit.classification?.get('id'),
-    );
-  }
-
-  get expiredExpectedEndDate() {
-    const expectedEndDate = this.model.administrativeUnit.expectedEndDate;
-    return expectedEndDate && expectedEndDate < new Date();
-  }
 }
