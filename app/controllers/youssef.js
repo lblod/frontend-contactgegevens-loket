1459c33a15db76c2f5fd710667aae4a721740294
youssef <youssef.jouhar1998@hotmail.com>
2023-11-06 18:00:11 +0100

    wip

--- app/controllers/youssef.js
+++ app/controllers/youssef.js
@@ -1,34 +1,53 @@
 import Controller from '@ember/controller';
-import { task } from 'ember-concurrency';
-import { FORM_GRAPHS } from '../routes/youssef';
+import { task, timeout } from 'ember-concurrency';
+import { action } from '@ember/object';
 
 export default class YoussefController extends Controller {
-  saveTask = task(async (event) => {
-    console.log('dit is boven de event preventdefault');
+  @task
+  *saveTask(event) {
+    // Prevent the default form submission behavior
     event.preventDefault();
-    console.log('dit is onder de prevent default', event.preventDefault());
+
     let { adminUnitId, siteId, formData } = this.model;
+
     console.log(
       `http://localhost:8000/semantic-forms/${adminUnitId}/form/${siteId}`,
     );
     console.log('dit is je siteid', siteId);
-    await this.saveFormData(adminUnitId, siteId, formData);
-  });
 
+    // Simulate an asynchronous operation (remove this part in your actual code)
+    yield timeout(1000);
+
+    yield this.saveFormData(adminUnitId, siteId, formData);
+  }
+
+  // Define the saveFormData function
   async saveFormData(adminUnitId, siteId, formData) {
     console.log(
       `http://localhost:8000/semantic-forms/${adminUnitId}/form/${siteId}`,
     );
 
-    await fetch(
-      `http://localhost:8000/semantic-forms/${adminUnitId}/form/${siteId}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-          'Content-Type': 'application/json',
+    try {
+      console.log('Dit is je siteID', siteId)
+      // Perform the HTTP request to save the form data
+      const response = await fetch(
+        `http://localhost:8000/semantic-forms/${adminUnitId}/form/${siteId}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(formData),
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    );
+      );
+
+      if (response.ok) {
+        console.log('success');
+      } else {
+        console.log('failed');
+      }
+    } catch (error) {
+      // Handle any network or request errors
+    }
   }
 }
