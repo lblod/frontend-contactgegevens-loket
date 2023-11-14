9d6f2b3b6cc30613c18a9a11d49b216163ee04a5
youssef <youssef.jouhar1998@hotmail.com>
2023-11-14 13:39:32 +0100

    WIP

--- app/controllers/sites/new.js
+++ app/controllers/sites/new.js
@@ -3,48 +3,82 @@ import { inject as service } from '@ember/service';
 import { combineFullAddress } from 'frontend-contactgegevens-loket/models/address';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
+import { errorValidation } from '../../validations/site-validation';
 
 export default class CreateSitesNewController extends Controller {
   @service router;
   @service store;
   @tracked isPrimarySite = false;
+  @tracked validationErrors = {};
 
   get isLoading() {
     return this.saveTask.isRunning || this.cancelTask.isRunning;
   }
 
-  saveTask = task(async (event) => {
-    event.preventDefault();
+  async validateData() {
     const { address, primaryContact, secondaryContact, site, adminUnit } =
       this.model;
-    address.fullAddress = combineFullAddress(address);
-    await primaryContact.save();
-    await secondaryContact.save();
-    await address.save();
-    site.contacts = [primaryContact, secondaryContact];
-    site.address = address;
-    await site.save();
-    const nonPrimarySites = await adminUnit.sites;
 
-    if (this.isPrimarySite) {
-      const previousPrimarySite = await adminUnit.primarySite;
-      if (previousPrimarySite) {
-        nonPrimarySites.push(previousPrimarySite);
+    const validationData = {
+      siteType: site.siteType.get('label'),
+      street: address.street,
+      country: address.country,
+      housenumber: address.number,
+      postcode: address.postcode,
+      municipality: address.municipality,
+      province: address.province,
+      fullAddress: address.fullAddress,
+      telephonePrimary: primaryContact.telephone,
+      emailPrimary: primaryContact.email,
+      websitePrimary: primaryContact.website,
+      telephoneSecondary: secondaryContact.telephone,
+    };
+    return errorValidation.validate(validationData);
+  }
+
+  saveTask = task(async (event) => {
+    event.preventDefault();
+    const validationResult = await this.validateData();
+    if (!validationResult.error) {
+      const { address, primaryContact, secondaryContact, site, adminUnit } =
+        this.model;
+
+      address.fullAddress = combineFullAddress(address);
+      await primaryContact.save();
+      await secondaryContact.save();
+      await address.save();
+      site.contacts = [primaryContact, secondaryContact];
+      site.address = address;
+      await site.save();
+
+      const nonPrimarySites = await adminUnit.sites;
+      if (this.isPrimarySite) {
+        const previousPrimarySite = await adminUnit.primarySite;
+        if (previousPrimarySite) {
+          nonPrimarySites.push(previousPrimarySite);
+        }
+        adminUnit.primarySite = site;
+      } else {
+        nonPrimarySites.push(site);
       }
-      adminUnit.primarySite = site;
-    } else {
-      nonPrimarySites.push(site);
-    }
 
-    await adminUnit.save();
-    this.router.transitionTo('sites.index'); // Model does not reload?
+      await adminUnit.save();
+      this.router.transitionTo('sites.index');
+    } else {
+      this.validationErrors = validationResult.error.details.reduce((errors, detail) => {
+        errors[detail.context.key] = detail.message;
+          return errors;
+        },
+        {},
+      );
+      console.log('Validation Errors:', this.validationErrors);
+    }
   });
 
   cancelTask = task(async (event) => {
     event.preventDefault();
-    const { address, primaryContact, secondaryContact, site, adminUnit } =
-      this.model;
-    // Destroy the newly created models
+    const { address, primaryContact, secondaryContact, site, adminUnit } = this.model;
+
     address.deleteRecord();
     await address.save();
     primaryContact.deleteRecord();
@@ -53,10 +87,9 @@ export default class CreateSitesNewController extends Controller {
     await secondaryContact.save();
     site.deleteRecord();
     await site.save();
-    // Roll back the changes to admin unit if any
-    adminUnit.rollback();
 
-    this.router.transitionTo('sites.index'); // Model does not reload?
+    adminUnit.rollback();
+    this.router.transitionTo('sites.index');
   });
 
   reset() {
---
+++ app/validations/site-validation.js
@@ -0,0 +1,47 @@
+import Joi from 'joi';
+
+const belgiumPhoneNumberRegex = /^(?:\+32|0)[4-9][0-9]{8}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const websiteRegex = /^https:\/\//;
+
+export const errorValidation = Joi.object({
+  siteType: Joi.string()
+    .required()
+    .messages({ '*': 'Please choose a site type' }),
+  country: Joi.string()
+    .required()
+    .messages({ '*': 'Please fill in a valid country' }),
+  street: Joi.string()
+    .required()
+    .messages({ '*': 'Please fill in a valid street name' }),
+  housenumber: Joi.string()
+    .required()
+    .messages({ '*': 'Please fill in a valid housenumber' }),
+  postcode: Joi.string()
+    .required()
+    .messages({ '*': 'Please fill in a postalcode' }),
+  municipality: Joi.string()
+    .required()
+    .messages({ '*': 'Please fill in a valid municipality' }),
+  province: Joi.string()
+    .required()
+    .messages({ '*': 'Please fill in a valid province' }),
+  fullAddress: Joi.string()
+    .required()
+    .messages({ '*': 'Please choose a address' }),
+  telephonePrimary: Joi.string()
+    .required()
+    .pattern(belgiumPhoneNumberRegex)
+    .messages({ '*': 'Please fill in a valid Belgium phone number' }),
+  emailPrimary: Joi.string()
+    .pattern(emailRegex)
+    .messages({ '*': 'Please fill in a valid email' }),
+  websitePrimary: Joi.string().optional().pattern(websiteRegex).messages({
+    '*': 'Please fill in a valid URL. Example: https://www.vlaanderen.be',
+  }),
+  telephoneSecondary: Joi.string()
+    .optional()
+    .allow('')
+    .pattern(belgiumPhoneNumberRegex)
+    .messages({ '*': 'Please fill in a valid Belgium phone number' }),
+}).options({ abortEarly: false });
