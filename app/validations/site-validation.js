3bfdeda97bdeec6a9defaa8f69e9d59034459880
youssef <youssef.jouhar1998@hotmail.com>
2023-11-21 11:44:35 +0100

    Revert "Validations finished"
    This reverts commit 2c290030e5e1f4ff3b6092ed62b40c173f76e16e.

--- app/validations/site-validation.js
+++ app/validations/site-validation.js
@@ -4,6 +4,24 @@ const belgiumPhoneNumberRegex = /^(?:\+32|0)[4-9][0-9]{8}$/;
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const websiteRegex = /^https:\/\//;
 
+const commonValidations = {
+  telephonePrimary: Joi.string()
+    .required()
+    .pattern(belgiumPhoneNumberRegex)
+    .messages({ '*': 'Please fill in a valid Belgium phone number' }),
+  emailPrimary: Joi.string()
+    .pattern(emailRegex)
+    .messages({ '*': 'Please fill in a valid email' }),
+  websitePrimary: Joi.string().optional().pattern(websiteRegex).messages({
+    '*': 'Please fill in a valid URL. Example: https://www.vlaanderen.be',
+  }),
+  telephoneSecondary: Joi.string()
+    .optional()
+    .allow('')
+    .pattern(belgiumPhoneNumberRegex)
+    .messages({ '*': 'Please fill in a valid Belgium phone number' }),
+};
+
 export const errorValidation = Joi.object()
   .keys({
     siteType: Joi.string()
@@ -66,8 +84,6 @@ export const warningValidation = Joi.object()
     telephoneSecondary: Joi.string()
       .optional()
       .pattern(belgiumPhoneNumberRegex)
-      .messages({
-        '*': 'Please fill in a valid Belgium phone number, are you sure you want to continue ?',
-      }),
+      .messages({ '*': 'Please fill in a valid Belgium phone number' }),
   })
   .options({ abortEarly: false });
