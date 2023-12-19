f1e146101ebba916dedba981bfed97aa337327dc
youssef <youssef.jouhar1998@hotmail.com>
2023-12-19 17:14:22 +0100

    wip

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,12 +1,13 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import ENV from '../../../config/environment.js';
+import ENV from 'frontend-contactgegevens-loket/config/environment';
 
 export default class AuthLoginRoute extends Route {
   @service session;
 
   beforeModel() {
     if (this.session.prohibitAuthentication('index')) {
+      console.log(ENV.acmidm);
       window.location.replace(buildLoginUrl(ENV.acmidm));
     }
   }
@@ -15,11 +16,10 @@ export default class AuthLoginRoute extends Route {
 function buildLoginUrl({ authUrl, clientId, authRedirectUrl, scope }) {
   let loginUrl = new URL(authUrl);
   let searchParams = loginUrl.searchParams;
-
-  searchParams.append('response_type', 'code');
-  searchParams.append('client_id', clientId);
-  searchParams.append('redirect_uri', authRedirectUrl);
-  searchParams.append('scope', scope);
+  searchParams.append('response_type', encodeURIComponent('code'));
+  searchParams.append('client_id', encodeURI frnComponent(clientId));
+  searchParams.append('redirect_uri', encodeURIComponent(authRedirectUrl));
+  searchParams.append('scope', encodeURIComponent(scope));
 
   return loginUrl.href;
 }
