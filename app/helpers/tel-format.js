dd7eca8133b888e02b618542ec1e2666d840942f
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-01-16 10:25:00 +0100

    Added type guarding for the tel-helper function

--- app/helpers/tel-format.js
+++ app/helpers/tel-format.js
@@ -1,10 +1,13 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function telFormat([tel]) {
-  if (tel) {
-    if (tel.includes('tel:')) {
-      tel = tel.split(':')[1];
-    }
-    return tel;
+  if (!tel) throw new Error('Tel format helper requires one parameter');
+
+  if (typeof tel !== 'string')
+    throw new Error('Tel format helper expects a string as a parameter.');
+
+  if (tel.includes('tel:')) {
+    tel = tel.split(':')[1];
   }
+  return tel;
 });
