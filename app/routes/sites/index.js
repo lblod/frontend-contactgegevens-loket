9634e33986300edd857c825147cec2bd87b0ca80
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-01-19 16:16:51 +0100

    Redirect user to login page when not authenticated

--- app/routes/sites/index.js
+++ app/routes/sites/index.js
@@ -4,15 +4,20 @@ import { A } from '@ember/array';
 export default class ContactDataViewSiteRoute extends Route {
   @service store;
   @service currentSession;
+  @service router;
 
   async model() {
     // TODO: Not every site seems to get an address? Why?
-    let sites = await this.currentSession.group.get('sites');
-    let primarySite = await this.currentSession.group.get('primarySite');
-    const allSites = A([primarySite, ...sites]);
-    return {
-      allSites,
-      primarySiteId: primarySite.id,
-    };
+    try {
+      let sites = await this.currentSession.group.get('sites');
+      let primarySite = await this.currentSession.group.get('primarySite');
+      const allSites = A([primarySite, ...sites]);
+      return {
+        allSites,
+        primarySiteId: primarySite.id,
+      };
+    } catch (error) {
+      this.router.transitionTo('auth.logout');
+    }
   }
 }
