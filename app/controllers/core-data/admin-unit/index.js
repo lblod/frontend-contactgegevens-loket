66f4c230e82803b249053ac7cf93f90a329d99ab
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2024-02-09 07:48:56 +0100

    Correction. Using a getter

--- app/controllers/core-data/admin-unit/index.js
+++ app/controllers/core-data/admin-unit/index.js
@@ -2,4 +2,8 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 export default class CoreDataAdminUnitIndexController extends Controller {
   @service currentSession;
+
+  get adminUnitNoKboAndDistrict() {
+    return !this.model.kbo && this.model.isDistrict;
+  }
 }
