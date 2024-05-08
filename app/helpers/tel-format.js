26ab03fcb696b441a31f4114406847e26f8eca7f
YJouhar47 <youssef.jouhar1998@hotmail.com>
2024-05-08 11:25:43 +0200

    WIP

--- app/helpers/tel-format.js
+++ app/helpers/tel-format.js
@@ -6,8 +6,54 @@ export default helper(function telFormat([tel]) {
   if (typeof tel !== 'string')
     throw new Error('Tel format helper expects a string as a parameter.');
 
+  let formattedTel = '';
+
+  // Remove 'tel:' if present
   if (tel.includes('tel:')) {
     tel = tel.split(':')[1];
   }
-  return tel;
+  console.log('before', tel);
+
+  // Remove all non-numeric characters
+  tel = tel.replace(/[^\d+]/g, '');
+  console.log('tel:', tel);
+  // Format according to specifications
+  if (tel.startsWith('0800')) {
+    // Special case: 0800 numbers
+    formattedTel += '0800 ';
+    tel = tel.slice(4);
+  } else {
+    // Normal numbers
+    if (tel.startsWith('00')) {
+      formattedTel += '+';
+      tel = tel.slice(2);
+    } else if (tel.startsWith('+')) {
+      formattedTel += '+';
+      tel = tel.slice(1);
+    }
+
+    if (tel.length >= 2) {
+      formattedTel += tel.slice(0, 2) + ' ';
+      tel = tel.slice(2);
+    }
+
+    if (tel.length === 4) {
+      formattedTel += tel;
+      return formattedTel;
+    }
+
+    if (tel.length % 2 !== 0) {
+      formattedTel += tel.slice(0, 3) + ' ';
+      tel = tel.slice(3);
+    }
+
+    while (tel.length > 2) {
+      formattedTel += tel.slice(0, 2) + ' ';
+      tel = tel.slice(2);
+    }
+  }
+
+  formattedTel += tel;
+
+  return formattedTel.trim();
 });
