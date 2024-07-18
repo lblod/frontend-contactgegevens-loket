05af449dd3e91620e00daa418f0987989d633f5f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-07-18 12:55:59 +0200

    Remove a broken test
    This test is outdated and it makes the test script run indefinitely.

--- tests/unit/validators/nested-validator-test.js
+++
@@ -1,8 +0,0 @@
-import { module, test } from 'qunit';
-import validateNestedValidator from 'frontend-contactgegevens-loket/validators/nested-validator';
-
-module('Unit | Validator | nested-validator');
-
-test('it exists', function (assert) {
-  assert.ok(validateNestedValidator());
-});
